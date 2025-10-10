810586a6054854ae57fe2c0034bb82b97da54722
SkyesRol <1771821463@qq.com>
2025-10-10 15:55:27 +0800

    navbar done,first commit of blog

---
+++ my_blog/src/routes/routes.tsx
@@ -0,0 +1,16 @@
+// routes.tsx
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+
+
+
+export function AppRoutes() {
+    return (
+
+        <Routes>
+            <Route path="/" element={<Home />} />
+
+        </Routes>
+
+    );
+}
